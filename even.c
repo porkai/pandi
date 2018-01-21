65e91d300cc0a92c7536b7bab94e2a4d54a8946e
porkai <35598411+porkai@users.noreply.github.com>
2018-01-21 10:36:09 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(void)  {
+	int a;
+    printf("enter tha value  a=");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+          printf(" even");
+    } 
+    else
+    {
+         printf("  odd");
+    }
+	return 0;
+}
