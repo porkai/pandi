ac0deb0265d63cb78e6a638002540ad220f6585a
porkai <35598411+porkai@users.noreply.github.com>
2018-02-02 10:47:30 +0000

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int a,n,d,s=0;
+	printf("enter tha value");
+	scanf("%d%d%d",&a,&d,&n);
+	s=n+(a*d*1)+(a*d*2);
+	{
+		printf("%d",&d);
+		
+	}
+	return 0;
+}
