fa6abe362b18808f26e286f7908a5aa7e5562444
porkai <35598411+porkai@users.noreply.github.com>
2018-02-07 15:47:43 +0530

    Create maxelement.c

---
+++ maxelement.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+main()
+{
+    int a,b,c,e;
+    printf("enter the num\n");
+    scanf("%d",&a);
+printf("enter the numbers\n",a);
+scanf("%d%d%d",&b,&c,&e);
+if(e>c)
+printf("%d",e);
+else if(e>b)
+printf("%d",e);
+else if(c>b)
+printf("%d",c);
+else if(c>e)
+printf("%d",c);
+else if(b>c)
+printf("%d",b);
+else if(b>e)
+printf("%d",b);
+}
