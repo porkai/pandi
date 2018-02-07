999e8a1b213b68f420d2e9347d2561ff93d61940
porkai <35598411+porkai@users.noreply.github.com>
2018-02-07 16:24:08 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+   char a;
+   scanf("%s",&a);
+   if(a>='0'&&a<='9')
+   {
+   	printf("yes");
+   }
+   else
+  {
+  	printf("no");
+  	
+  
+   }
+	return 0;
+}
