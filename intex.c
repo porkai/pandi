ae88bf68b01b7eb29385cdafb3ec9495772edc82
porkai <35598411+porkai@users.noreply.github.com>
2018-02-07 16:07:28 +0530

    Create intex.c

---
+++ intex.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int i,n,arr[i];
+	printf("enter tha number of element");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d%d",arr[i],i);
+	}
+	
+	
+	
+	return 0;
+}
